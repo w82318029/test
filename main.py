659656bcb6654f8da7cbe9c00247d1cfa92ea544
w82318029 <w82318029@163.com>
2022-01-21 16:47:53 +0800

    add the sum of a & b

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 #coding=utf-8
 
-print ("Life is short, you need Python !")//原文出自【易百教程】，商业转载请联系作者获得授权，非商业请保留原文链接：https://www.yiibai.com/git/git_perform_changes.html
+print ("Life is short, you need Python !")
+
+a = 10
+b = 20
+
+c = a + b
+print("The value of c is  ", c)
+
 
