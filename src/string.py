d47570a90f2ac1acd29608c311bf352badde4850
w82318029 <w82318029@163.com>
2022-01-26 14:28:30 +0800

    string.py add a function and some variable

--- src/string.py
+++ src/string.py
@@ -6,5 +6,11 @@ var2 = "Python Programming"
 print ("var1[0]: ", var1[0])
 print ("var2[1:5]: ", var2[1:5]) # 切片加索引
 
-def stract():
-    return
+def my_strcat(str1, str2):
+    return (str1+str2)
+
+a = '我'
+b = 'ab'
+ab = '我ab'
+
+print(len(a), len(b), len(ab), len('='))
